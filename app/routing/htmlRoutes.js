e8af53b7fc6292d0541804da1ca715d7e16a1a2e
VinTran <tranquillo.vinnie@gmail.com>
2019-11-15 10:06:56 -0500

    Nodemon installed

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+var express = require("express")
+var path = require("path")
+
+var app = express();
+
+module.exports = function (app) {
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+};
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 // Configure the Express application
 var app = express();
@@ -16,9 +16,9 @@ app.use(bodyParser.text());
 
 // Add the application routes
 // require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 // Start listening on PORT
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log("Pokemon is listening on PORT: " + PORT);
 });
