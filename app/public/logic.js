50fd69f47aa60e238da1fe3f6da71a4a19fd41ff
VinTran <tranquillo.vinnie@gmail.com>
2019-11-27 10:19:49 -0500

    Updates

--- app/public/logic.js
+++ app/public/logic.js
@@ -1,43 +1,66 @@
-// var queryURL = "https://pokeapi.co/api/v2/pokemon";
+// getPoke();
 
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log("It worked");
-//   //console.log(response);
-// });
+// function getPoke() {
+//   var queryURL = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
 
-getPoke(); 
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     for (var i = 0; i < response.results.length; i++) {
+//       var name = response.results[i].name;
+//       var namediv = $("<div>");
+//       var caps = name.toUpperCase();
 
-function getPoke(){
-var queryURL = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
+//       var p = $("<p>").text(caps);
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
+//       $(namediv).append(p);
 
-  for (var i = 0; i < response.results.length; i++){
-    var name = response.results[i].name;
-    var namediv = $("<div>");
-    var caps = name.toUpperCase(); 
-    
+//       $("#test").prepend(namediv);
+//     }
+//     console.log(response); 
+//   });
+// }
 
-    var p = $("<p>").text(caps); 
+const poke = {
+  pokePerPage: 10,
+  currentPage: 1,
+  results:null
+};
 
-    $(namediv).append(p); 
+const pagination = document.querySelector('.pages');
+const output = document.querySelector('.output');
 
-    console.log(name); 
-    console.log(caps);
+const init = function(){
+  console.log("ready");
+  const url = 'https://randomuser.me/api/?results=66'; 
 
-    $("#test").prepend(namediv);
-  }
-
- 
-
-});
-
+  fetch(url).then(function(res){
+    return res.json()
+  }).then(function(data){
+    poke.results = data.results;
+    console.log(poke);
+    loadPage(1); 
+  })
 }
 
+const loadPage = function(pg){
+  poke.currentPage = pg;
+  let startPoke = (poke.currentPage-1) * poke.pokePerPage; 
+  let totalPages = Math.ceil(poke.results.length / poke.pokePerPage); 
+  console.log(totalPages); 
+  console.log(startPoke); 
+  
+  output.innerHTML = `<h1>Page ${poke.currentPage}<h1`; 
 
+  for(let i = 0; i < poke.pokePerPage; i++){
+    console.log(poke.results[i]);
+    let div = document.createElement('div');
+    div.innerHTML=`${poke.results[i].name.first}<br>`
+    output.appendChild(div);
+  }
+}
+
+window.addEventListener('load',function(){
+  init(); 
+})
