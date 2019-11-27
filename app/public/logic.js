af84c74ce13f30e46d59991dd6f856a92bc980a0
VinTran <tranquillo.vinnie@gmail.com>
2019-11-27 16:30:55 -0500

    Styling in progress and refactoring

--- app/public/logic.js
+++ app/public/logic.js
@@ -16,77 +16,69 @@ const init = function() {
       return res.json();
     })
     .then(function(data) {
-      const next = data.next; 
-      window.next2 = next; 
+      const next = data.next;
+      window.next2 = next;
       poke.results = data.results;
       console.log(data);
       loadPage(1);
     });
 };
 
-const getNext = function(){
-console.log(next2)
-fetch(next2)
+const getNext = function() {
+  console.log(next2);
+  fetch(next2)
     .then(function(res) {
       return res.json();
     })
     .then(function(data) {
-      const next = data.next; 
-      window.next2 = next; 
-      const prev = data.previous; 
-      window.prev2 = prev; 
+      const next = data.next;
+      window.next2 = next;
+      const prev = data.previous;
+      window.prev2 = prev;
       poke.results = data.results;
       console.log(data);
       console.log(data.next);
       console.log(poke);
       loadPage(1);
     });
-}
+};
 
-
-const getPrev = function(){
-  console.log(prev2)
+const getPrev = function() {
+  console.log(prev2);
   fetch(prev2)
-      .then(function(res) {
-        return res.json();
-      })
-      .then(function(data) {
-        const prev = data.previous; 
-        window.prev2 = prev; 
-        poke.results = data.results;
-        console.log(data);
-        console.log(data.next);
-        console.log(poke);
-        loadPage(1);
-      });
-  }
-
-
-
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      const prev = data.previous;
+      window.prev2 = prev;
+      poke.results = data.results;
+      console.log(data);
+      console.log(data.next);
+      console.log(poke);
+      loadPage(1);
+    });
+};
 
 const loadPage = function(pg) {
   poke.currentPage = pg;
-  pagination.innerHTML = ''; 
+  pagination.innerHTML = "";
   let startPoke = (poke.currentPage - 1) * poke.pokePerPage;
-  let totalPages = Math.ceil(poke.results.length / poke.pokePerPage);
   let endPoke =
     startPoke + poke.pokePerPage > poke.results.length
       ? poke.results.length
       : startPoke + poke.pokePerPage;
 
-  output.innerHTML = `<h1>Pokemon!<h1`;
+  
 
-  let pageOutput = document.createElement('div'); 
 
   for (let i = startPoke; i < endPoke; i++) {
     let Name = poke.results[i].name;
     let capName = Name.charAt(0).toUpperCase() + Name.slice(1);
     let div = document.createElement("div");
     div.innerHTML = `${capName}<br>`;
-    output.appendChild(div);
+    output.append(div);
   }
-
-  pagination.appendChild(pageOutput); 
 };
 
 window.addEventListener("load", function() {
