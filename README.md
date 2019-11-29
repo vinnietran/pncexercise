7de2be0a88f6bf0dc814c949cfbc2c38396b3b9b
VinTran <tranquillo.vinnie@gmail.com>
2019-11-29 09:06:17 -0500

    Limiting results to 4 per page and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pncexercise
+# Poke Pagination!
+
+This application uses Node.js along with the PokeAPI. 
+
+Using server side pagination, simply click the next or previous button to view the Pokemon available from the API
+
+This was my first attempt at any sort of pagination, and I like the way things have turned out!
--- app/public/logic.js
+++ app/public/logic.js
@@ -1,5 +1,5 @@
 const poke = {
-  pokePerPage: 20,
+  pokePerPage: 4,
   currentPage: 1,
   results: null
 };
@@ -7,9 +7,9 @@ const poke = {
 const pagination = document.querySelector(".pages");
 const output = document.querySelector(".output");
 
+//function to load the results based on the initial API call
 const init = function() {
-  console.log("ready");
-  const url = "https://pokeapi.co/api/v2/pokemon";
+  const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=4";
 
   fetch(url)
     .then(function(res) {
@@ -19,13 +19,12 @@ const init = function() {
       const next = data.next;
       window.next2 = next;
       poke.results = data.results;
-      console.log(data);
       loadPage(1);
     });
 };
 
+//function that gets the next URL from the API results on click of the next button in the UI
 const getNext = function() {
-  console.log(next2);
   fetch(next2)
     .then(function(res) {
       return res.json();
@@ -36,15 +35,12 @@ const getNext = function() {
       const prev = data.previous;
       window.prev2 = prev;
       poke.results = data.results;
-      console.log(data);
-      console.log(data.next);
-      console.log(poke);
       loadPage(1);
     });
 };
 
+//function that gets the previous URL from the API results on click of the previous button in the UI
 const getPrev = function() {
-  console.log(prev2);
   fetch(prev2)
     .then(function(res) {
       return res.json();
@@ -53,15 +49,13 @@ const getPrev = function() {
       const prev = data.previous;
       window.prev2 = prev;
       poke.results = data.results;
-      console.log(data);
-      console.log(data.next);
-      console.log(poke);
       loadPage(1);
     });
   output.innerHTML = "";
 };
 
 const loadPage = function(pg) {
+
   poke.currentPage = pg;
   $("#pokeTable").text("");
   let startPoke = (poke.currentPage - 1) * poke.pokePerPage;
@@ -73,7 +67,7 @@ const loadPage = function(pg) {
   for (let i = startPoke; i < endPoke; i++) {
     // pulling out URL to individual pokemon
     let url = poke.results[i].url;
-    console.log("Here is URL " + url);
+
     //using that URL for a new fetch to get the image
     fetch(url)
       .then(function(res) {
@@ -82,33 +76,34 @@ const loadPage = function(pg) {
       .then(function(data) {
         const pic = data.sprites.front_default;
         window.pic2 = pic;
-        console.log(pic);
 
+        // generating the sprite from URl
         var spriteDiv = $("<div>");
         spriteDiv.addClass("sprite");
-
         var img = $("<img />");
         img.addClass("sprite");
         img.attr("src", pic2);
-
         spriteDiv.append(img);
-
 
         //creating a new row for each poke
         var newRow = $("<tr>").append(
           $("<td>").text(capName),
           $("<td>").append(spriteDiv)
         );
+
         //adding new row to the table
         $("#pokeTable").append(newRow);
       });
+
     //getting the name of each poke
     let Name = poke.results[i].name;
+
     //converting string to caps
     let capName = Name.charAt(0).toUpperCase() + Name.slice(1);
   }
 };
 
+// event listener to start the function init when the page loads
 window.addEventListener("load", function() {
   init();
 });
